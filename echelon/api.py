907f87f0fc0587f8298673c4a9c939251daa5610
diadochos <takeshi.diadochos@gmail.com>
2021-10-16 18:49:03 +0900

    Add initial development code

---
+++ echelon/api.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+from dataclasses import dataclass
+
+from .algorithms import find_peak_echelons, find_foundation_echelons, find_echelon_clusters
+from .oracle import EchelonOracleBase, NdarrayEchelonOracle
+
+## Type hinting
+from typing import Tuple, List
+IndexSetType = List[int]
+EchelonType = IndexSetType
+EchelonsType = List[EchelonType]
+NeighborsType = List[IndexSetType]
+
+
+@dataclass
+class Result_EchelonAnalysis:
+    """Class for keeping the results of echelon analysis."""
+    peak_echelons: EchelonsType
+    foundation_echelons: EchelonsType
+    oracle: EchelonOracleBase
+
+@dataclass
+class Result_EchelonCluster:
+    table: object
+
+
+class EchelonAnalysis:
+    def cluster(self, result: Result_EchelonAnalysis) -> Result_EchelonCluster:
+        """
+        Examples:
+            >>>h, W = _mock_1dim_data()
+            >>>analyzer = EchelonAnalysis()
+            >>>result = analyzer(h, W)
+            >>>analyzer.cluster(result)
+              representatives                               indices
+            0            [16]  [16, 17, 15, 14, 18, 19, 20, 21, 22]
+            1            [13]               [13, 12, 14, 11, 10, 9]
+            2             [6]                    [6, 5, 7, 4, 8, 9]
+            3             [3]                       [3, 2, 4, 1, 0]
+            4            [23]                          [23, 22, 24]
+        """
+        clusters = find_echelon_clusters(result.peak_echelons, result.foundation_echelons, result.oracle)
+        echelon_clusters = []
+        for cluster in clusters:
+            _argmax, _ = result.oracle.max_indices(cluster)
+            echelon_clusters.append((_argmax, cluster))
+        echelon_cluster_table = pd.DataFrame(echelon_clusters, columns =['representatives', 'indices'])
+        return Result_EchelonCluster(
+            table=echelon_cluster_table
+        )
+
+    def __call__(self, data: np.ndarray, adjacency: np.ndarray) -> Result_EchelonAnalysis:
+        """
+        Params:
+            data: 1-dimensional data of realized values.
+            adjacency: size-$len(data)$ square array of adjacency.
+
+        Examples:
+            >>>h, W = _mock_1dim_data()
+            >>>EchelonAnalysis()(h, W)
+            Result_EchelonAnalysis(peak_echelons=[[16, 17, 15], [13], [6, 5, 7], [3], [23]], foundation_echelons=[[12, 14, 18, 19, 11, 10, 20], [2, 4, 8], [1, 9, 21], [0, 22, 24]])
+        """
+        oracle = NdarrayEchelonOracle(data, adjacency)
+        peak_echelons = find_peak_echelons(oracle)
+        foundation_echelons = find_foundation_echelons(oracle, peak_echelons)
+        return Result_EchelonAnalysis(
+            peak_echelons=peak_echelons,
+            foundation_echelons=foundation_echelons,
+            oracle=oracle
+        )
+
+
+class OneDimEchelonAnalysis(EchelonAnalysis):
+    def __call__(self, data: np.ndarray) -> Result_EchelonAnalysis:
+        """
+        Params:
+            data: 1-dimensional data of realized values.
+
+        Examples:
+            >>>h, _ = _mock_1dim_data()
+            >>>OneDimEchelonAnalysis()(h)
+            Result_EchelonAnalysis(peak_echelons=[[16, 17, 15], [13], [6, 5, 7], [3], [23]], foundation_echelons=[[12, 14, 18, 19, 11, 10, 20], [2, 4, 8], [1, 9, 21], [0, 22, 24]])
+        """
+        W = np.zeros((len(data),)*2, dtype='int8')
+        W[np.eye(len(W), k=1, dtype='bool')] = 1
+        W[np.eye(len(W), k=-1, dtype='bool')] = 1
+        return super().__call__(data, W)
---
+++ echelon/oracle.py
@@ -0,0 +1,109 @@
+## Typing
+from typing import Tuple, Any, List
+IndexType = int
+import numpy as np
+
+
+def _lists_intersection(list1, list2) -> list:
+    return list(set(list1).intersection(list2))
+
+
+def _mock_pytest_data():
+    n = 5
+    v = np.array([5, 3, 2, 5, 4])
+    w = np.zeros((n, n))
+    w[np.eye(len(w), k=1, dtype='bool')] = 1
+    w[np.eye(len(w), k=-1, dtype='bool')] = 1
+    oracle = NdarrayEchelonOracle(v, w)
+    return v, w
+
+
+class EchelonOracleBase:
+    def __init__(self, max_of_empty:Any=-np.inf):
+        """Constructor.
+
+        Params:
+            max_of_empty: value to return if the indices list is empty.
+        """
+        self.max_of_empty = max_of_empty
+
+    def copy_indices(self) -> List[IndexType]:
+        pass
+    def nb(self, indices: List[IndexType]) -> List[IndexType]:
+        pass
+    def max_indices(self, indices: List[IndexType]) -> Tuple[List[IndexType], Any]:
+        """
+        Params:
+            indices: indices to query.
+        """
+        pass
+    def find_peaktop(self, indices: List[IndexType]) -> Tuple[IndexType, Any]:
+        """
+        Examples:
+            >>>NdarrayEchelonOracle(*_mock_pytest_data()).find_peaktop([1, 2, 3])
+            (3, 5)
+        """
+        _argmax_list, _max = self.max_indices(indices)
+        return _argmax_list[0], _max
+
+    def max_nb(self, indices: List[IndexType]) -> Tuple[List[IndexType], Any]:
+        """
+        Examples:
+            >>>NdarrayEchelonOracle(*_mock_pytest_data()).max_nb([1, 2])
+            ([0, 3], 5)
+        """
+        nb_indices = self.nb(indices)
+        return self.max_indices(nb_indices)
+
+    def max_nb_indices(self, ind1, ind2):
+        inds = _lists_intersection(self.nb(ind1), ind2)
+        return self.max_indices(inds)
+
+    def pop_extend_family(self, indices, families):
+            _family_i = indices
+            for i, family in enumerate(families):
+                if _lists_intersection(self.nb(indices), family):
+                    _family_i += family
+                    del families[i]
+            return _family_i, families
+
+class NdarrayEchelonOracle(EchelonOracleBase):
+    def __init__(self, values: np.ndarray, adjacency: np.ndarray):
+        """Constructor.
+
+        Params:
+            data: 1-dimensional array of the observed values for the grids.
+            adjacency: 2-dimensional adjacency matrix for the grids.
+        """
+        super().__init__()
+        assert len(values) == len(adjacency)
+        self.values = values
+        self.adjacency = adjacency
+
+    def copy_indices(self) -> List[IndexType]:
+        return list(range(len(self.values)))
+
+    def nb(self, indices: List[IndexType]) -> List[IndexType]:
+        """
+        Examples:
+            >>>NdarrayEchelonOracle(*_mock_pytest_data()).nb([1, 2])
+            [0, 3]
+        """
+        _adj = self.adjacency[indices]
+        _adj = np.logical_or.reduce(_adj)
+        _neighbors = list(np.nonzero(_adj)[0])
+        return list(set(_neighbors)-set(indices))
+
+    def max_indices(self, indices: List[IndexType]) -> Tuple[List[IndexType], Any]:
+        """
+        Examples:
+            >>>NdarrayEchelonOracle(*_mock_pytest_data()).max_indices([1, 2])
+            ([1], 3)
+        """
+        if not indices:
+            return [], self.max_of_empty
+
+        vals = self.values[indices]
+        _max = np.max(vals)
+        _argmax_list = [indices[i] for i in np.flatnonzero(vals == _max)]
+        return _argmax_list, _max
